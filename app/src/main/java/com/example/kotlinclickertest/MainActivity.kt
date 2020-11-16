30c93cfea11d842fef5329e6e19face938305e51
EloxFire <enzo.avagliano@ynov.com>
2020-11-16 14:58:18 +0100

    Made new interface + auto increment

--- app/src/main/java/com/example/kotlinclickertest/MainActivity.kt
+++ app/src/main/java/com/example/kotlinclickertest/MainActivity.kt
@@ -2,15 +2,24 @@ package com.example.kotlinclickertest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.round
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
 class MainActivity : AppCompatActivity() {
 
     var upIf = 0
     var upWhile = 0
     var upAuto = 0
-    var ktls = 0
-    var kps = 1
+    var ktls:Float = 0f
+    var kpc = 1
+    var kps:Float = 1f
+
+    val mainHandler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,25 +30,43 @@ class MainActivity : AppCompatActivity() {
             counter.text = ktls.toString()
         }
 
+        totalKPC.text = "[KPC] Kotlins par click : " + kpc
+        totalKPS.text = "[KPS] Kotlins par secondes : " + kps
+
         upgrade1.setOnClickListener{
             multiplierTwo()
+            totalKPC.text = "[KPC] Kotlins par click : " + kpc
         }
 
         upgrade2.setOnClickListener{
             multiplierFour()
+            totalKPC.text = "[KPC] Kotlins par click : " + kpc
         }
+
+        upgrade3.setOnClickListener{
+            increaseKPS()
+            totalKPS.text = "[KPS] Kotlins par seconde : " + kps
+        }
+
+        mainHandler.post(object : Runnable {
+            override fun run() {
+                ktls += kps
+                counter.text = ktls.roundToInt().toString()
+                mainHandler.postDelayed(this, 1000)
+            }
+        })
 
     }
 
     public fun add() {
-        ktls += kps
+        ktls += kpc
         println(ktls)
     }
 
     public fun multiplierTwo(){
         if(ktls >= 10){
             ktls -= 10
-            kps = kps*2
+            kpc = kpc*2
             counter.text = ktls.toString()
             upIf += 1
             nbrIf.text = upIf.toString()
@@ -51,7 +78,7 @@ class MainActivity : AppCompatActivity() {
     public fun multiplierFour(){
         if(ktls >= 40){
             ktls -= 40
-            kps = kps*4
+            kpc = kpc*4
             counter.text = ktls.toString()
             upWhile += 1
             nbrWhile.text = upWhile.toString()
@@ -60,11 +87,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    public fun kotlinPerSecondOne(){
+    public fun increaseKPS(){
         if(ktls >= 40){
             ktls -= 40
-            kps = kps*4
-            counter.text = ktls.toString()
+            kps += kps*0.5f
+            upAuto += 1
+            nbr_ai.text = upAuto.toString()
         }else{
             return
         }
